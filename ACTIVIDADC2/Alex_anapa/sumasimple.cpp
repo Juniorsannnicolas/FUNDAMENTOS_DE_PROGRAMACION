e165c7df694eeab19690a164b538388b554f9dd5
juniorsannnicolas <alex.anapa.sannicolas@utelvt.edu.ec>
2021-12-15 16:04:01 -0500

    mi actividadc2 alexanapa

---
+++ ACTIVIDADC2/Alex_anapa/sumasimple.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float a,b,r;
+	cout<<" por favor ingrese el numero a :";
+	cin>>a;
+	
+	cout<<" por favor ingrese el numero b :";
+	cin>>b;
+ 
+	r=a+b;
+
+	cout<<" El resultado de la suma es : "<<r<<endl;
+	return 0;
+
+
+}	
+	
