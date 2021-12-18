7f77907818ebd56797b9e58f73350f76a3431ffa
juniorsannnicolas <alex.anapa.sannicolas@utelvt.edu.ec>
2021-12-18 17:57:58 -0500

    trabajo de alex

---
+++ ACTIVIDADC2/SUMA_DE_VARIO_NUMERO_ALEX_ANAPA/SUMAVARIONUMERIO.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+    int main()
+
+{
+	int a=0,b;
+	float z=0,x;
+	cout<<"por favor ingrese la cantidad de valore a suma : ";
+	cin>>b;
+	do{
+		cout<<" por favor ingrese el numeros : "<<a+1;
+		cin>>x;
+		a=a+1;
+		z=z+x;
+	}while(a<b);
+	
+	cout<<" la suma de vario numeros es : "<<z<<endl;
+	return 0;
+
+}		
