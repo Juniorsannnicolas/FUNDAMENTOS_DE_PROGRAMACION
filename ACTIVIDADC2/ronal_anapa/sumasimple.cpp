77582afa9ae75873bb0a76d1b9210401daf4a51d
Juniorsannnicolas <alex.anapa.sannicolas@utelvt.edu.ec>
2021-12-18 23:47:20 -0500

    aki esta mi trabajo ronal anapa

---
+++ ACTIVIDADC2/ronal_anapa/sumasimple.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace sted;
+int main()
+{
+float x,y,z;
+cout<<" ingrese el numero x : ";
+cin>>x;
+
+cout<<" ingrese el numero y : ";
+cin>>y;
+z=x+y;
+cout<<" el resultado de la suma : "<<z<< endl;
+return 0
+}
+
