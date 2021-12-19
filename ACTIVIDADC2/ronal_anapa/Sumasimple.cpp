b93328e708e4dff4b8efdc0997a891e06377549d
Juniorsannnicolas <alex.anapa.sannicolas@utelvt.edu.ec>
2021-12-19 17:01:17 -0500

    subiendo mi trabajo ronal

---
+++ ACTIVIDADC2/ronal_anapa/Sumasimple.cpp
@@ -0,0 +1,25 @@
+// haciendo suma de dos numeros//
+
+
+//Ronal anapa//
+
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+float x,y,z;
+cout<<" ingrese el numero x: ";
+cin>>x;
+
+cout<<" ingrese el numero y: ";
+cin>>y;
+z=x+y;
+cout<<" el resultado es : "<<z<<endl;
+return 0;
+}
+
