7ace4a440733bf2df1001867efe27f162512e237
juniorsannnicolas <alex.anapa.sannicolas@utelvt.edu.ec>
2021-12-18 19:55:20 -0500

    mi trabajo completo ACTIVIDADC2

---
+++ ACTIVIDADC2/ALEX_ANAPA_COMPARA_DOS_NUMEROS/comparadosnumero.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+      float a,b;
+
+              cout<<" Por favor ingrese un numero: "; cin>>a;
+	      cout<<" Por favor ingrese segundo numero: ";cin>>b;
+
+
+              if (a==b){ 
+		      
+
+	      cout<<"El valor de a= "<<a<<" Es igual a b= "<<b<<endl;
+      }else{
+	      if(a<b){
+	      cout<<"El valor de a="<<a<<" Es menor a b= " <<b<<endl;
+      }else{
+	      cout<<" El valor de b="<<b<<" Es menor a= "<<a<<endl;
+
+  }
+}
+     return 0;
+  
+}
